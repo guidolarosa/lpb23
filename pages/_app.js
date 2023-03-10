46e53b70627b2ace9e19215f54dc8243b0048441
guidolarosa <guido.glarosa@gmail.com>
2023-03-10 02:55:07 +0100

    Added styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import { Space_Mono } from 'next/font/google';
+
+const spaceMono = Space_Mono({subsets: ['latin'], weight: ['400']});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={spaceMono.className}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
