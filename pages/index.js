7bf112b83db484abf6ca910684156624ef65199a
guidolarosa <guido.glarosa@gmail.com>
2023-03-10 03:30:19 +0100

    Added some crazy styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { createClient } from 'contentful-management'
 
 
@@ -29,6 +29,7 @@ export default function Home(props) {
   });
 
   const postMessage = (message) => {
+    setMessageSendState('SENDING')
     client.getSpace(props.CONTENTFUL_SPACE_ID)
       .then((space) => space.getEnvironment('master'))
       .then((environment) => environment.createEntry('message', {
@@ -39,22 +40,21 @@ export default function Home(props) {
         }
       }))
       .then((entry) => {
-        setMessageSendState('SUCCESS');
-        getRandomMessage();
+        getRandomMessage(() => {
+          setMessageSendState('SUCCESS');
+        });
       })
       .catch((error) => {
         setMessageSendState('FAIL')
       })
   };
 
-  const getRandomMessage = () => {
+  const getRandomMessage = (onComplete) => {
     getAllMessages((messages) => {
       let totalMessages = messages.length;
       let randomNumber = getRandomInt(0, totalMessages);
-      console.log(randomNumber)
-      console.log(messages[randomNumber - 1]);
       setReceivedMessage(messages[randomNumber].fields.messageContent['en-US']);
-
+      onComplete();
     })
   }
 
@@ -85,13 +85,14 @@ export default function Home(props) {
       </Head>
 
       <StyledMain>
+        <div className="overlay"/>
         <div className="content">
           <form 
             className={`${formDisabled ? 'disabled' : ''}`} 
             onSubmit={handleFormSubmit}
           >
             <div className="form-group">
-              <label id="message">Mensaje</label>
+              <label id="message">TU MENSAJE</label>
               <input 
                 type="text" 
                 name="message"
@@ -105,19 +106,23 @@ export default function Home(props) {
             <button type="submit">Enviar</button>
           </form>
         </div>
-        {receivedMessage && (
-          <strong className="received-message">{receivedMessage}</strong>
-        )}
-        {/* <strong>Tremendo mensaje larguísimo que vamos a user para probar si se ve bien un mensaje tan largo como este</strong> */}
-         {/* <Image
-          src="/giphy.gif"
-          fill
-          alt="background"
-        /> */}
+        <strong className="received-message">
+          {messageSendState == 'NOT SENT' && 'CONECTANDO CON LA INTERFAZ CUÁNTICA'}
+          {messageSendState == 'SENDING' && 'RECIBIENDO MENSAJE...'}
+          {messageSendState == 'SUCCESS' && receivedMessage}
+        </strong>
       </StyledMain>
     </div>
   )
 }
+
+const colorChange = keyframes`
+  0% {background-color: limegreen}
+  25% {background-color: magenta}
+  50% {background-color: yellow}
+  75% {background-color: cyan}
+  100% {background-color: limegreen}
+`;
 
 const StyledMain = styled.main`
   display: flex;
@@ -129,6 +134,17 @@ const StyledMain = styled.main`
   position: relative;
   background-image: url('/giphy.gif');
   background-size: 20rem;
+  /* background: black; */
+  .overlay {
+    width: 100vw;
+    height: 100vh;
+    pointer-events: none;
+    position: absolute;
+    z-index: 2;
+    background: limegreen;
+    mix-blend-mode: multiply;
+    animation: ${colorChange} 5s infinite forwards;
+  }
   .content {
     position: relative;
     z-index: 1;
@@ -136,6 +152,7 @@ const StyledMain = styled.main`
     padding: 2rem;
     width: 30rem;
     max-width: 80vw;
+    border: 1px solid white;
     form {
       display: flex;
       flex-direction: column;
@@ -174,20 +191,31 @@ const StyledMain = styled.main`
         label {
           margin-bottom: 0.5rem;
           text-align: center;
+          font-size: 2rem;
         }
       }
     }
   }
   .received-message {
-    margin-top: 2rem;
+    /* margin-top: 2rem; */
     color: white;
-    display: inline-block;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     text-align: center;
     font-size: 2rem;
     width: 30rem;
     max-width: 80vw;
     background: black;
     padding: 2rem;
+    min-height: 20rem;
+    border: 1px solid white;
+    border-top: 0;
+    background-image: url('/waves.gif');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    text-transform: uppercase;
   }
 `;
 
